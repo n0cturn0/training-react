02c1bc7b40610069bcd086c846f09501ecb067a1
Luiz Augusto <n0cturn0.debian@gmail.com>
2023-02-23 15:23:37 -0400

    Aula sobre Jsx

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Luiz Augusto ';
+  const email = 'n0cturn0.debian@gmail.com';
+  const newname = name.toUpperCase();
+  function sum(a,b)
+  {
+    return a+b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
   return (
     <div className="App">
      Hello Word!
+     <p>Olá {newname}</p>
+     <p>{email}</p>
+     <p>{10/5}</p>
+     <p>Soma: {sum(90,7654)}</p>
+     <img src={url} />
     </div>
   );
 }
