abbe08210a79dcb7bc99ddccedf8aeaab078d47c
Luiz Augusto <n0cturn0.debian@gmail.com>
2023-02-24 10:09:18 -0400

    useState Biblioteca do react, importante para trabalhar com formulário, ajuda na manipulação de dados dos formulários

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,39 @@
+import { useState } from "react";
 function Form(){
     function cadastraUsuario(e){
         e.preventDefault()
+        console.log(` Usuario ${name} foi cadastrado com a senha ${password}`)
         console.log('Cadastrou o usuario')
     }
+
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
+
+
     return(
         <div>
             <h1>Meu Cadastro</h1>
             <form onSubmit={cadastraUsuario}>
                 <div>
-                <input type="text" placeholder="Digite seu nome" />
+                <label htmlFor="name"></label>
+                <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                placeholder="Digite seu nome"
+                onChange={(e) => setName(e.target.value)}
+                />
+                </div>
+
+                <div>
+                <label htmlFor="password"></label>
+                <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                placeholder="Digite uma senha"
+                onChange={(e) => setPassword(e.target.value)}
+                />
                 </div>
                 <div>
                 <input type="submit" value="Cadastrar" />
